1fa39d67807b36ce84cd2a2a20c5ce1694c6880e
Abdullah Al Fahim <abfahimb@gmail.com>
2024-11-01 06:26:49 +0600

    EncryptStoreJS

---
+++ README.md
@@ -0,0 +1,38 @@
+# encrypt-storejs
+
+**Encrypt-storejs** is a lightweight JavaScript library designed for securely storing data in the browser's local storage. By leveraging advanced encryption techniques, it ensures that sensitive information remains safe from unauthorized access.
+
+## Table of Contents
+
+- [Description](#description)
+- [Why You Need This Package](#why-you-need-this-package)
+- [Where You Can Use It](#where-you-can-use-it)
+- [How to Use](#how-to-use)
+  - [Installation](#installation)
+  - [Basic Usage](#basic-usage)
+  - [Advanced Usage](#advanced-usage)
+- [How to Contribute](#how-to-contribute)
+- [License](#license)
+
+## Description
+
+`encrypt-storejs` simplifies the process of encrypting and storing data in local storage, making it a robust solution for modern web applications that prioritize security. With a straightforward API, developers can easily integrate secure storage solutions without sacrificing usability.
+
+## Why You Need This Package
+
+In today's digital landscape, protecting user data is paramount. Local storage is often exploited by malicious actors to gain access to sensitive information. `encrypt-storejs` provides a seamless way to secure data storage in the browser, helping to prevent data breaches and enhancing user trust in your application.
+
+## Where You Can Use It
+
+- Single-page applications (SPAs) where sensitive user data is stored locally.
+- Applications that require secure user authentication and session management.
+- Any web application that needs to maintain user preferences or configurations securely.
+
+## How to Use
+
+### Installation
+
+To get started, install the package using npm:
+
+```bash
+npm install encrypt-storejs
---
+++ src/encrypt-store.ts
@@ -0,0 +1,149 @@
+export interface AppData {
+  [key: string]: any;
+}
+
+export class EncryptStoreJS<T extends AppData> {
+  private static instance: EncryptStoreJS<any>;
+  private key: string;
+  private storageKey: string;
+
+  private constructor(storageKey: string, password: string) {
+      this.storageKey = storageKey;
+      this.key = password;
+  }
+
+  public static init<T extends AppData>(storageKey: string, password: string): EncryptStoreJS<T> {
+      if (!EncryptStoreJS.instance) {
+        EncryptStoreJS.instance = new EncryptStoreJS<T>(storageKey, password);
+      } else {
+          console.warn('SecureLocalStorage has already been initialized. Please create a new instance for different settings.');
+      }
+      return EncryptStoreJS.instance as EncryptStoreJS<T>;
+  }
+
+  private generateRandomSalt(length: number = 16): string {
+      const randomBytes = new Uint8Array(length);
+      window.crypto.getRandomValues(randomBytes);
+      return Array.from(randomBytes, byte => String.fromCharCode(byte)).join('');
+  }
+
+  private expandKey(key: string, salt: string): string {
+      const expandedKey: string[] = [];
+      for (let i = 0; i < Math.max(key.length, salt.length); i++) {
+          const keyChar = key.charCodeAt(i % key.length);
+          const saltChar = salt.charCodeAt(i % salt.length);
+          expandedKey.push(String.fromCharCode(keyChar ^ saltChar));
+      }
+      return expandedKey.join('');
+  }
+
+  private xorEncryptDecrypt(data: string): string {
+      const salt = this.generateRandomSalt();
+      const expandedKey = this.expandKey(this.key, salt);
+      let result = '';
+
+      for (let i = 0; i < data.length; i++) {
+          result += String.fromCharCode(data.charCodeAt(i) ^ expandedKey.charCodeAt(i % expandedKey.length));
+      }
+
+      return this.encodeToBase64(salt + result);
+  }
+
+  private encodeToBase64(data: string): string {
+      try {
+          return btoa(unescape(encodeURIComponent(data)));
+      } catch (error) {
+          this.clearEncryptStore();
+          throw error;
+      }
+  }
+
+  private decodeFromBase64(data: string): string {
+      try {
+          return decodeURIComponent(escape(atob(data)));
+      } catch (error) {
+          this.clearEncryptStore();
+          throw error;
+      }
+  }
+
+  public encryptData(data: string): string {
+      return this.xorEncryptDecrypt(data);
+  }
+
+  public decryptData(encryptedData: string): string {
+      const decoded = this.decodeFromBase64(encryptedData);
+      const salt = decoded.substring(0, 16);
+      const encryptedMessage = decoded.substring(16);
+      const expandedKey = this.expandKey(this.key, salt);
+      let result = '';
+
+      for (let i = 0; i < encryptedMessage.length; i++) {
+          result += String.fromCharCode(encryptedMessage.charCodeAt(i) ^ expandedKey.charCodeAt(i % expandedKey.length));
+      }
+
+      return result;
+  }
+
+  public setEncryptStore(value: T): void {
+      if (typeof value === 'object' && value !== null) {
+          const encodedData = this.encryptData(JSON.stringify(value));
+          if (typeof window !== 'undefined') {
+              localStorage.setItem(this.storageKey, encodedData);
+          }
+      } else {
+          console.error('Value must be an object');
+      }
+  }
+
+  public updateEncryptStore(updates: Partial<T>): void {
+      if (typeof window !== 'undefined') {
+          const encodedData = localStorage.getItem(this.storageKey);
+          const storedData = encodedData ? JSON.parse(this.decryptData(encodedData)) as T : {};
+
+          if (typeof updates === 'object' && updates !== null) {
+              const updatedData = { ...storedData, ...updates };
+              const newEncodedData = this.encryptData(JSON.stringify(updatedData));
+              localStorage.setItem(this.storageKey, newEncodedData);
+          } else {
+              console.error('Updates must be an object');
+          }
+      }
+  }
+
+  public deleteEncryptStore(fields: Array<keyof T>): void {
+      if (typeof window !== 'undefined') {
+          const encodedData = localStorage.getItem(this.storageKey);
+          const storedData = encodedData ? JSON.parse(this.decryptData(encodedData)) as T : {};
+
+          if (storedData && typeof storedData === 'object') {
+              fields.forEach(field =>   delete (storedData as any)[field]);
+              const newEncodedData = this.encryptData(JSON.stringify(storedData));
+              localStorage.setItem(this.storageKey, newEncodedData);
+          } else {
+              console.error('Stored data is not an object or does not exist');
+          }
+      }
+  }
+
+  public getEncryptStore(field?: keyof T): T | null {
+      if (typeof window !== 'undefined') {
+          const encodedData = localStorage.getItem(this.storageKey);
+          const parsedData = encodedData ? JSON.parse(this.decryptData(encodedData)) as T : null;
+
+          if (field && parsedData && field in parsedData) {
+              return parsedData[field] || null;
+          }
+
+          return parsedData;
+      }
+
+      return null;
+  }
+
+  public clearEncryptStore(): void {
+      if (typeof window !== 'undefined') {
+          localStorage.removeItem(this.storageKey);
+      }
+  }
+}
